892c431495a04affa71879ad59afabba410ff816
Julien Lay <julien-lay@outlook.fr>
2023-12-12 13:01:43 +0100

    cours matin 12-12-23
    controller, model, routes

---
+++ app/backend/server.js
@@ -0,0 +1,25 @@
+// Import des modules
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+// Couleur dans la console
+const colors = require('colors')
+
+// Connexion à MongoDB
+const connectDB = require('./config/db')
+connectDB()
+
+// Initialisation d'Express
+const app = express()
+
+// Accepter les données envoyées par formulaire
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// Routes
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`)
+})
